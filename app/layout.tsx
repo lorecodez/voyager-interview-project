0cae9904358b9dda65e14e60f1aa96f24a63d683
lorecodez <lorenzo.washington233@gmail.com>
2024-05-16 15:48:38 -0700

    setting header and footer and loading animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { lazy, Suspense } from "react";
+import Spinner from '@/app/components/Spinner';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +11,14 @@ export const metadata: Metadata = {
   description: "Lorenzo's interview project for the Voyager Program",
 };
 
+const Foot = lazy(() => import('@/app/components/Foot').then( module => {
+  return {default: module.default}
+}))
+
+const Head = lazy(() => import('@/app/components/Head').then( module => {
+  return {default: module.default}
+}))
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +26,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <Suspense fallback={
+            <Spinner/>
+          }>
+            <Head/>
+          </Suspense>
+        </header>
+        {children}
+        <footer>
+          <Suspense fallback={
+            <Spinner/>
+          }>
+            <Foot/>
+          </Suspense>
+        </footer>
+      </body>
     </html>
   );
 }
