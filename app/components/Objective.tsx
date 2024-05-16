1205688da581da48310acb1022f6460836c1d15e
lorecodez <lorenzo.washington233@gmail.com>
2024-05-16 15:54:41 -0700

    added title and objective

---
+++ app/components/Objective.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//short section to summerize my goals, qualifications and what I bring to the program
+export default function Objective() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ app/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//must include name and be at the top of the page
+export default function Title() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
