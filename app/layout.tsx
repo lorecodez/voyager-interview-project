8cedbb71b4202a92acd867c4b9ad30cac3cfae8d
lorecodez <lorenzo.washington233@gmail.com>
2024-05-24 14:34:36 -0700

    fixing font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Inter, Baskervville } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { lazy, Suspense } from "react";
 import Spinner from '@/app/components/Spinner';
 
 const inter = Inter({ subsets: ["latin"] });
 
-const baskervville = Baskervville({ 
-  subsets: ["latin"], 
-  weight: ['400'], 
-  style: ['normal', 'italic'],
-  variable: '--font-baskervville'
-})
 
 export const metadata: Metadata = {
   title: "Voyager Interview Project",
@@ -33,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`w-full h-full min-h-screen ${inter.className} ${baskervville.variable}`}>
+      <body className={`w-full h-full min-h-screen ${inter.className} `}>
         <header className="w-full h-full p-2 sticky top-0 z-40 flex justify-center">
           <Suspense fallback={
             <Spinner/>
